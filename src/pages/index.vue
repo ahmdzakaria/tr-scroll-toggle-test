31715f12ec360f1878d82b2edd85a82313421f78
ahmdzakaria <azakaria921@gmail.com>
2026-01-14 17:11:43 +0700

    chore: setup index page base

---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="padding: 24px">
+    <h1>Memulai Belajar Vue</h1>
+    <p>Step 1 done.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
